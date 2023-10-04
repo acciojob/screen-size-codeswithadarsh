a4fd4af4aa0c83492a25cfb304e8b25b6167c6cf
Adarsh Pandey <97337633+codeswithadarsh@users.noreply.github.com>
2023-10-04 05:39:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+const sizeInfo = document.createElement("div");
+sizeInfo.id = "sizeInfo";
+// sizeInfo.style.height = "100vh";
+// sizeInfo.style.width = "100vw";
+document.body.appendChild(sizeInfo);
+const h1 = document.createElement("h1");
+sizeInfo.appendChild(h1);
+
+
+
+
+var refresh = setInterval(resolution,0);
+
+function resolution (){
+    // var screenSize = window.screen;
+    h1.textContent = `Width: ${window.innerWidth} and Height: ${window.innerHeight}`;
+}
